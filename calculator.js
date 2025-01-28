b8e1eab251c5ad07941b1ab6fd2a31d36d05fc2b
bytesandcookiesindore <bytesandcookiesindore@gmail.com>
2025-01-28 16:57:02 +0530

    single number test case

--- calculator.js
+++ calculator.js
@@ -2,4 +2,5 @@ export function add(numbers) {
     if (numbers === '') {
         return 0;
     }
+    return parseInt(numbers, 10);
 }
--- test/test.js
+++ test/test.js
@@ -5,5 +5,9 @@ describe('String Calculator', function() {
     it('should return 0 for an empty string', function() {
         expect(add('')).to.equal(0);
     });
+
+    it('should return the number for a single number', function() {
+        expect(add('1')).to.equal(1);
+    });
 });
 
